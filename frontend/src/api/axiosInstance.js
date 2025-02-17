d8d3dd4096067bdd1f23a288cbd1fda093922612
safayet35 <sf.safayet35@gmail.com>
2025-02-18 02:45:00 +0600

    update frontend baseurl

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://uno-counter-bue4.onrender.com/api/v1",
   withCredentials: true, // Enables sending cookies
   headers: {
     "Content-Type": "application/json"
